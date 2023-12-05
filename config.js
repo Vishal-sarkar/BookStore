606d5cd0b276e0373cbaf88393bd06134f416da7
Vishal-sarkar <vishalsarkar852@gmail.com>
2023-12-05 16:31:15 +0530

    Changes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 5555;
 
-export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.p3jmlrf.mongodb.net/nooks-collection?retryWrites=true&w=majority'
+export const mongoDBURL = process.env.MDB_URL || 'mongodb+srv://root:root@book-store-mern.p3jmlrf.mongodb.net/nooks-collection?retryWrites=true&w=majority'
